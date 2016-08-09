2f93212724450da2585c7ec4201330cd135c8b51
yadong.zhang <yadong.zhang0415@gamil.com>
2016-08-09 17:43:50 +0800

    update

--- src/main/java/com/test/interceptor/MyFilter.java
+++ src/main/java/com/test/interceptor/MyFilter.java
@@ -32,7 +32,7 @@ public class MyFilter implements Filter {
 	/**
 	 * 默认限制时间（单位：ms）
 	 */
-	private static final long LIMITED_TIME_MILLIS = 1 * 60 * 1000;
+	private static final long LIMITED_TIME_MILLIS = 60 * 60 * 1000;
 
 	/**
 	 * 用户连续访问最高阀值，超过该值则认定为恶意操作的IP，进行限制
